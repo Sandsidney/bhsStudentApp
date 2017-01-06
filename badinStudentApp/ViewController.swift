cb1757f14d36c56e7de67ee6f255e309f8f3806a
BHSRam9 <sandsidn@badinhs.org>
2017-01-05 22:50:36 -0500

    printing in console finished, method counting, all view controller connections connected

--- badinStudentApp/ViewController.swift
+++ badinStudentApp/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
     
     var countingNum = 0
     
-    
+//counting method 
     
     
     
@@ -36,47 +36,56 @@ class ViewController: UIViewController {
     @IBAction func bellScheduale(_ sender: UIButton) {
         countingNum = countingNum+1
         print(countingNum)
+        print("Bell Scheduale Button Pushed")
     }
     
     @IBAction func clubsActivities(_ sender: UIButton) {
         countingNum = countingNum+1
         print(countingNum)
+        print("Clubs & Activities Button Pushed")
     }
     
     @IBAction func spiritShop(_ sender: UIButton) {
         countingNum = countingNum+1
         print(countingNum)
+        print("Spirit Shop Button Pushed")
     }
     
     @IBAction func studentMonth(_ sender: UIButton) {
         countingNum = countingNum+1
         print(countingNum)
+        print("Student of the Month Button Pushed")
     }
     
     @IBAction func mmProgram(_ sender: UIButton) {
         countingNum = countingNum+1
         print(countingNum)
+        print("M&M Program Button Pushed")
     }
     
     @IBAction func dailyAnnounc(_ sender: UIButton) {
         countingNum = countingNum+1
         print(countingNum)
+        print("Daily Announcements Button Pushed")
     }
     
     @IBAction func cafMenu(_ sender: UIButton) {
         countingNum = countingNum+1
         print(countingNum)
+        print("Cafetiria Menu Button Pushed")
     }
     
     @IBAction func scholarOpp(_ sender: UIButton) {
         countingNum = countingNum+1
         print(countingNum)
+        print("Scholarship Opportunities Button Pushed")
     }
     
     
     @IBAction func calculator(_ sender: UIButton) {
         countingNum = countingNum+1
         print(countingNum)
+        print("Calculator Button Pushed")
     }
     
     
